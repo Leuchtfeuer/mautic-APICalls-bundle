99419857510229bc444ddeefcd97dd7ca026b4ff
achmedsagitov <sagitovakhmed@mail.ru>
2025-09-03 15:06:05 +0200

    [TASK] refactoring  CampaignActionSubscriber [MTC-8344]

--- EventListener/CampaignActionSubscriber.php
+++ EventListener/CampaignActionSubscriber.php
@@ -5,21 +5,17 @@ namespace MauticPlugin\LeuchtfeuerAPICallsBundle\EventListener;
 use Mautic\CampaignBundle\CampaignEvents;
 use Mautic\CampaignBundle\Event\CampaignBuilderEvent;
 use Mautic\CampaignBundle\Event\PendingEvent;
-use Mautic\LeadBundle\Helper\CustomFieldHelper;
-use Mautic\LeadBundle\Helper\TokenHelper;
-use Mautic\LeadBundle\Model\LeadModel;
 use MauticPlugin\LeuchtfeuerAPICallsBundle\Form\Type\ApiRequestActionType;
 use MauticPlugin\LeuchtfeuerAPICallsBundle\LeuchtfeuerAPICallsEvents;
-use MauticPlugin\LeuchtfeuerAPICallsBundle\Services\ApiCallsService;
+use MauticPlugin\LeuchtfeuerAPICallsBundle\Services\ContactProcessorService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CampaignActionSubscriber implements EventSubscriberInterface
 {
     public const ACTION_TYPE = 'mautic.leuchtfeuer.api_request';
 
 
-    public function __construct(private ApiCallsService $apiCallsService){}
+    public function __construct(private ContactProcessorService $contactProcessorService){}
     public static function getSubscribedEvents(): array
     {
         return [
@@ -41,24 +37,15 @@ class CampaignActionSubscriber implements EventSubscriberInterface
         );
     }
 
-
     public function onExecuteApiRequest(PendingEvent $event): void
     {
-        $contacts = $event->getContacts();
-        $properties   = $event->getEvent()->getProperties();
-
-        foreach ($contacts as $contact) {
-
-                $tokenizedValue = TokenHelper::findLeadTokens($properties['body'], $contact->getProfileFields(), true);
-                $this->apiCallsService->sendRequest($tokenizedValue, $properties['method'], $properties['url']);
+        try {
+            $this->contactProcessorService->processContacts($event->getContacts(), $event->getEvent()->getProperties());
+            $event->passAll();
+        } catch (\Throwable $e) {
+            $event->failAll($e->getMessage());
         }
-
-            try {
-                $event->pass();
-            } catch (\Throwable $e) {
-                $event->fail();
-            }
-        }
+    }
 
 
 
