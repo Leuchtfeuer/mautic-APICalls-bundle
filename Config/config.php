6ed43b4fc9086a5f8d29ffe7ae46aba3fcd327b2
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2026-02-05 17:09:29 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -9,7 +9,7 @@ use MauticPlugin\LeuchtfeuerAPICallsBundle\Integration\Support\ConfigSupport;
 return [
     'name'        => 'API Calls by Leuchtfeuer',
     'description' => 'Allow generic outbound API calls e.g. as campaign action',
-    'version'     => '6.0.0',
+    'version'     => '6.0.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'services'    => [
         'integrations' => [
