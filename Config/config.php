91a7e1677bb98e24a89d8e951766219dc4b38b95
Madlen <m.friedrich@leuchtfeuer.com>
2026-09-11 08:34:52 +0200

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'name'        => 'API Calls by Leuchtfeuer',
     'description' => 'Allow generic outbound API calls e.g. as campaign action',
-    'version'     => '6.1.0',
+    'version'     => '7.0.0',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
 ];
